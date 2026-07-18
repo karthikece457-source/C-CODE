74f055c5500449190e64a326c9d1fbe6f69bc701
karthik k <karthikece457@gmail.com>
2026-07-18 10:10:12 +0530

    Arithemetic.C

---
+++ Arithemetic.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    printf("enter the a value :");
+    scanf("%d",&a);
+    printf("enter the b value :");
+    scanf("%d",&b);
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+    return 0;
+}
